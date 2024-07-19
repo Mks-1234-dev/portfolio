1ff375bb585acc00fea6a96f231c9a25fb4d98c5
Mks-1234-dev <seemasatya2@gmail.com>
2024-07-19 17:27:40 +0530

    9 commit

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -7,43 +7,58 @@ import Link from "next/link";
 
 const HeroSection = () => {
   return (
-    <section>
-      <div>
-        <h1>
-          <span>Hello, I&apos;m </span>
-          <br></br>
-          <TypeAnimation
-            sequence={[
-              "Mks",
-              1000,
-              "CS professional",
-              1000,
-              "Web Developer",
-              1000,
-              "App Developer",
-              1000,
-            ]}
-            wrapper="span"
-            speed={50}
-            repeat={Infinity}
-          />
-        </h1>
-        <p>Currently unl student majoring in computer engineering,fresher.</p>
-        <div>
-          <Link href="/#contact">Contact Me</Link>
-          <a href="link">
-            <span>View CV</span>
-          </a>
-        </div>
-
-        <div>
-          <Image
-            src="/images/new.png"
-            alt="portfolio image"
-            width={600}
-            height={300}
-          />
-        </div>
+    <section className="lg:py-16">
+      <div className="grid grid-cols-1 sm:grid-cols-12">
+        <motion.div
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.5 }}
+          className="col-span-8 place-self-center text-center sm:text-left justify-self-start"
+        >
+          <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-8xl lg:leading-normal font-extrabold">
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-primary-400 to-secondary-600">
+              Hello, I&apos;m{" "}
+            </span>
+            <br></br>
+            <TypeAnimation
+              sequence={[
+                "Mks",
+                1000,
+                "CS professional",
+                1000,
+                "Web Developer",
+                1000,
+                "App Developer",
+                1000,
+              ]}
+              wrapper="span"
+              speed={50}
+              repeat={Infinity}
+            />
+          </h1>
+          <p>Currently unl student majoring in computer engineering,fresher.</p>
+          <div>
+            <Link href="/#contact">Contact Me</Link>
+            <a href="link">
+              <span>View CV</span>
+            </a>
+          </div>
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.5 }}
+          className="col-span-4 place-self-center mt-4 lg:mt-0"
+        >
+          <div>
+            <Image
+              src="/images/new.png"
+              alt="portfolio image"
+              width={600}
+              height={300}
+            />
+          </div>
+        </motion.div>
       </div>
     </section>
   );
