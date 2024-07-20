f89ef63e5cd3bd2b47a809c12824491fcd21c5ac
Mks-1234-dev <seemasatya2@gmail.com>
2024-07-20 13:46:18 +0530

    28 commit

---
+++ src/app/components/EmailSection.jsx
@@ -0,0 +1,62 @@
+"use client";
+import React, { useState } from "react";
+import GithubIcon from "../../../public/github-icon.svg";
+import Link from "next/link";
+import Image from "next/image";
+
+const EmailSection = () => {
+  const [emailSubmitted, setEmailSubmitted] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const data = {
+      email: e.target.email.value,
+      subject: e.target.subject.value,
+      message: e.target.message.value,
+    };
+    const JSONdata = JSON.stringify(data);
+    const endpoint = "/api/send";
+
+    // Form the request for sending data to the server.
+    const options = {
+      // The method is POST because we are sending data.
+      method: "POST",
+      // Tell the server we're sending JSON.
+      headers: {
+        "Content-Type": "application/json",
+      },
+      // Body of the request is the JSON data we created above.
+      body: JSONdata,
+    };
+
+    const response = await fetch(endpoint, options);
+    const resData = await response.json();
+
+    if (response.status === 200) {
+      console.log("Message sent.");
+      setEmailSubmitted(true);
+    }
+  };
+
+  return (
+    <section id="contact">
+      <div></div>
+      <div>
+        <h5>Let&apos;s Connect</h5>
+        <p>
+          {" "}
+          I&apos;m currently looking for new opportunities, my inbox is always
+          open. Whether you have a question or just want to say hi, I&apos;ll
+          try my best to get back to you!
+        </p>
+        <div>
+          <Link href="https://github.com/">
+            <Image src={GithubIcon} alt="Github Icon" />
+          </Link>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default EmailSection;
